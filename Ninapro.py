de2ccd054120b4a90af8c2b779ae605e58dca049
Rich Cohen <rchchn@gmail.com>
2022-04-18 19:51:32 -0400

    working on db1

--- Ninapro.py
+++ Ninapro.py
@@ -15,6 +15,7 @@ class Database:
         returns the data as a pandas dataframe
         '''
         dataset = loadmat(path)
+        
         header = dataset.pop("__header__")
         version = dataset.pop("__version__")
         globs = dataset.pop("__globals__")
@@ -24,11 +25,28 @@ class Database:
     def LoadEMGDataset(self, path):
         dataset = loadmat(path)
         data = pd.DataFrame(dataset['emg'])
-        data['stimulus'] = dataset['restimulus'] 
+        data['stimulus'] = dataset['stimulus'] 
+        data['restimulus'] = dataset['restimulus'] 
         data['repetition'] = dataset['repetition']
+        data['rerepetition'] = dataset['rerepetition']
 
         self.DataSet = data        
         return data
+    
+    def FilterDataset(self, stimulus, repetition, reannotated=False): 
+        if reannotated:
+            stim = self.DataSet[(self.DataSet.stimulus==stimulus) & (self.DataSet.repetition==repetition) & 
+                                (self.DataSet.restimulus>=0) & (self.DataSet.rerepetition>=0)] 
+        else:
+            
+            stim = self.DataSet[(self.DataSet.stimulus==stimulus) & (self.DataSet.repetition==repetition) & 
+                                (self.DataSet.restimulus==stimulus) & (self.DataSet.rerepetition==1)] 
+             
+        stim.pop('stimulus')        
+        stim.pop('repetition')
+        stim.pop('restimulus')        
+        stim.pop('rerepetition') 
+        return stim.to_numpy().T 
 
     def SetCurrentDataset(self, dataset):
         self.DataSet = dataset
@@ -45,6 +63,17 @@ class Dataset:
         
     def GetCurrentDataset(self):
         return self.DataSet
+    
+    def FilterDataset(self, stim=None, rep=None, filterRe=False, save=False):
+        if filterRe==None:
+            temp=self.DataSet[(self.DataSet['stimulus']==stim) & (self.DataSet['repetition']==rep)]
+        else:
+            temp=self.DataSet[(self.DataSet['restimulus']==stim) & (self.DataSet['rerepetition']==rep)]
+            
+        if save:
+            self.DataSet = temp
+        return temp
+        
         
     def FilterStimulus(self, stim, save=False):
         isTrue = self.DataSet['stimulus']==stim
@@ -54,6 +83,18 @@ class Dataset:
     
     def FilterRepetition(self, rep, save=False):
         isTrue = self.DataSet['repetition']==rep
+        if save:
+            self.DataSet = self.DataSet[isTrue]
+        return self.DataSet[isTrue]
+        
+    def FilterReStimulus(self, stim, save=False):
+        isTrue = self.DataSet['restimulus']==stim
+        if save:
+            self.DataSet = self.DataSet[isTrue]
+        return self.DataSet[isTrue]
+    
+    def FilterReRepetition(self, rep, save=False):
+        isTrue = self.DataSet['rerepetition']==rep
         if save:
             self.DataSet = self.DataSet[isTrue]
         return self.DataSet[isTrue]
